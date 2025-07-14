07a8020b10956732deca6003bd6d6705cf01b459
percy garcia <pgarciavilla123@gmail.com>
2025-07-13 22:57:30 -0500

    c99

--- automation_exercise/Pages/Bacciones.cs
+++ automation_exercise/Pages/Bacciones.cs
@@ -11,7 +11,7 @@ namespace automation_exercise.Pages
     public class Bacciones
     {
         private IWebDriver _driver;
-        IWebElement linksingin => _driver.FindElement(By.LinkText("aaaaaign In"));
+        IWebElement linksingin => _driver.FindElement(By.LinkText("Sign In"));
 
         public Bacciones(IWebDriver driver)
         {
