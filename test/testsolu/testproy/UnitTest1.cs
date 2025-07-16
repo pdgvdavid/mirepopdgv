fd8a9a5bcc445fe984c928eabf5a07261c1c124c
percy garcia <pgarciavilla123@gmail.com>
2025-07-16 17:20:46 -0500

    c1

---
+++ test/testsolu/testproy/UnitTest1.cs
@@ -0,0 +1,80 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+///using OpenQA.Selenium.Chrome;
+
+namespace testproy
+{
+    public class Tests
+    {
+        private IWebDriver _driver; 
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new EdgeDriver();
+           //_driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("D:\\a\\mirepopdgv\\mirepopdgv\\introduccion.html");
+           // _driver.Navigate().GoToUrl("C:\\aCICD\\introduccion.html");
+
+
+            TestContext.WriteLine("=== PAGE SOURCE ===");
+            TestContext.WriteLine(_driver.PageSource);
+            TestContext.WriteLine("=== END PAGE SOURCE ===");
+            _driver.Manage().Window.Maximize();
+           
+        }
+
+        [Test]
+        public void Test1()
+        {
+            bool flag = false;
+//            System.Threading.Thread.Sleep(3000);
+WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+wait.Until(driver => driver.FindElement(By.Id("multiplicando")));
+
+            IWebElement input1 = _driver.FindElement(By.Id("multiplicando"));
+            IWebElement input2 = _driver.FindElement(By.Id("multiplicador"));
+            IWebElement botoncalcula = _driver.FindElement(By.Id("btnproducto"));
+            IWebElement fieldproducto = _driver.FindElement(By.Id("producto"));
+            
+            input1.SendKeys("5");
+            input2.SendKeys("6");
+            botoncalcula.Click();
+            System.Threading.Thread.Sleep(3000);
+
+            //if (fieldproducto.Text == "30")
+            if (fieldproducto.GetAttribute("value") == "30")
+
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = false;
+            }
+
+            Assert.IsTrue(flag);
+
+
+           
+            
+        }
+
+        [TearDown]
+        public void cerrar()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver.Dispose();
+            
+            }
+        }
+
+
+
+
+
+
+    }
+}
